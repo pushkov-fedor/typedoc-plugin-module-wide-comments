4a7e1e54fcb03b725bc110737f8de4c643690365
Fedor Pushkov <f.pushkov@g.nsu.ru>
2020-07-09 16:06:15 +0700

    Add comments to index.ts

--- index.ts
+++ index.ts
@@ -5,15 +5,19 @@ import { ReflectionKind } from 'typedoc/dist/lib/models/reflections/abstract'
 import { DeclarationReflection } from 'typedoc'
 
 export function load({application}: PluginHost) {
+  /** Subscribe on EVENT_CREATE_DECLARATIN. */
   application.converter.on(Converter.EVENT_CREATE_DECLARATION, (context: Context, reflection: DeclarationReflection, node) => {
+    /** Re-define all Modules as External. */
     reflection.kind = reflection.kind === ReflectionKind.Module ? ReflectionKind.ExternalModule : reflection.kind
     if (!node) {
       return
     }
+    /** Get comment from current node, which declaration is being created. */
     const rawComment = getRawComment(node)
     if (!rawComment) {
       return
     }
+    /** If comment was added (e.g. for function, constants etc) - do nothing , otherwise (for modules) add parsed comment. */
     reflection.comment = !reflection.comment ? parseComment(rawComment) : reflection.comment
   })
 }
